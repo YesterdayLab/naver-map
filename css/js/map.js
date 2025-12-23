93a9ed5fd1fca94bb42f93e5537e0c92f3c69437
Chang M. Yun <147887720+chang-m-yun@users.noreply.github.com>
2025-12-22 21:07:52 -0800

    Create map initialization with marker and info window in map.js
    Initialize a map with a marker and info window for a specific location.

---
+++ css/js/map.js
@@ -0,0 +1,43 @@
+(function () {
+  var position = new naver.maps.LatLng(POSITION.lat, POSITION.lng);
+  var isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+  var map = new naver.maps.Map('map', {
+    center: position,
+    zoom: 16,
+    zoomControl: true,
+    zoomControlOptions: {
+      style: isMobile
+        ? naver.maps.ZoomControlStyle.SMALL
+        : naver.maps.ZoomControlStyle.LARGE,
+      position: isMobile
+        ? naver.maps.Position.LEFT_BOTTOM
+        : naver.maps.Position.LEFT_CENTER
+    }
+  });
+
+  var marker = new naver.maps.Marker({
+    position: position,
+    map: map
+  });
+
+  var infoWindow = new naver.maps.InfoWindow({
+    content: `
+      <div class="naver-label">
+        서울아산병원 장례식장: ${ROOM_NAME}
+        <div class="naver-address">
+          서울 송파구 올림픽로43길 88
+        </div>
+        <a class="naver-link" href="${NAVER_URL}" target="_blank">
+          > 네이버 지도 바로가기
+        </a>
+      </div>
+    `,
+    borderWidth: 0,
+    backgroundColor: 'transparent',
+    anchorSize: new naver.maps.Size(0, 0),
+    pixelOffset: new naver.maps.Point(0, -28)
+  });
+
+  infoWindow.open(map, marker);
+})();
